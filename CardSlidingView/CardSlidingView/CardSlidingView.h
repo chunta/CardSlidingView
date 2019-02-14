bb9583cdf45bb88038ef46d605802276605f7d40
Rex <rex.chen@appledaily.com.tw>
2019-02-14 14:09:25 +0800

    First Framework

---
+++ CardSlidingView/CardSlidingView/CardSlidingView.h
@@ -0,0 +1,19 @@
+//
+//  CardSlidingView.h
+//  CardSlidingView
+//
+//  Created by nmi on 2019/2/14.
+//  Copyright © 2019 nmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardSlidingView.
+FOUNDATION_EXPORT double CardSlidingViewVersionNumber;
+
+//! Project version string for CardSlidingView.
+FOUNDATION_EXPORT const unsigned char CardSlidingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardSlidingView/PublicHeader.h>
+
+
---
+++ CardSlidingView/CardSlidingView/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  CardSlidingView
+//
+//  Created by nmi on 2019/2/14.
+//  Copyright © 2019 nmi. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init(){}
+    
+    public static func doSomething()->String {
+        return "Did Some Stuff..."
+    }
+}
