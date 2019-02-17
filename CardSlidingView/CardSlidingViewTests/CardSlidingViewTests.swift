255e102d5c716c146faeb5fb87fad470f61edb1e
Rex <rex.chen@appledaily.com.tw>
2019-02-17 10:44:54 +0800

    Test fix

--- CardSlidingView/CardSlidingViewTests/CardSlidingViewTests.swift
+++ CardSlidingView/CardSlidingViewTests/CardSlidingViewTests.swift
@@ -12,7 +12,6 @@ class CardSlidingViewTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
     }
 
     override func tearDown() {
